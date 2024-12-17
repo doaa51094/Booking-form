8d2b098305cbf8053c8508e03370762d510531a9
doaa51094 <112436865+doaa51094@users.noreply.github.com>
2024-12-17 23:35:43 +0200

    add strip puplish key

--- src/components/FormForPayment.jsx
+++ src/components/FormForPayment.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
-import { Elements, CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 
-const stripePromise = loadStripe("your-publishable-stripe-key");
+const stripePromise = loadStripe(
+  "pk_test_51QX8J8H2VHxHqwNVVEVWLLDzvDBRtGxsIIKKIZIQaai2spcpm4Hx066T0jls0TNuJqwEYrPUIGfgNcP6cULxWjWn00HMOuAH9O"
+);
 
 const CheckoutForm = () => {
   const stripe = useStripe();
@@ -29,10 +36,9 @@ const CheckoutForm = () => {
   };
 
   return (
-      <div className="card-element-container">
-        <CardElement options={{ hidePostalCode: true }} />
-      </div>
- 
+    <div className="card-element-container">
+      <CardElement options={{ hidePostalCode: true }} />
+    </div>
   );
 };
 
